fd9851a063b7f1f0658545844838419f2c60310e
joe6894 <kellyjoseph18@gmail.com>
2020-11-05 11:14:23 +0000

    added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from  products.models import Product
+from products.models import Product
 
 def bag_contents(request):
 
@@ -17,7 +17,7 @@ def bag_contents(request):
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
             })
         else:
@@ -27,10 +27,10 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
-            })
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
